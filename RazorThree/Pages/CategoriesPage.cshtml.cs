8997d278debb14a7d2f872a002c34b5370e819b4
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-09-07 03:37:02 +0200

    Added validations and alerts to the client side.

--- RazorThree/Pages/CategoriesPage.cshtml.cs
+++ RazorThree/Pages/CategoriesPage.cshtml.cs
@@ -5,6 +5,7 @@ using Grpc.Net.Client;
 using Google.Protobuf.WellKnownTypes;
 using System.Net.Http;
 using System.Threading.Tasks.Dataflow;
+using System.Xml.Linq;
 
 namespace RecipeRazor.Pages
 {
@@ -12,7 +13,6 @@ namespace RecipeRazor.Pages
     {
         [BindProperty]
         public List<CategoryItem> categories { get; set; } = new();
-        public List<string> msg { get; set; } = new();
         private readonly Recipe.RecipeClient _recipeServiceClient;
         private readonly Category.CategoryClient _categoryServiceClient;
 
@@ -22,59 +22,122 @@ namespace RecipeRazor.Pages
             _categoryServiceClient = categoryServiceClient;
         }
 
-        public async Task<IActionResult> OnGet(List<string> messages)
+        // Listing categories.
+        public async Task<IActionResult> OnGet()
         {
             var categoriesGroup = await _categoryServiceClient.ListCategoriesAsync(new());
             categories = categoriesGroup.SingleCategory.ToList();
-            msg = messages;
             return Page();
         }
 
-        public async Task<IActionResult> OnPostDelete(string deletedValue)
+        // Deleting a category.
+        public async Task<IActionResult> OnPostDelete(string categoryToDelete)
         {
-            int index = Int32.Parse(deletedValue);
-            var categoriesGroup = await _categoryServiceClient.ListCategoriesAsync(new());
-            categories = categoriesGroup.SingleCategory.ToList();
-            var c = await _categoryServiceClient.RemoveCategoriesAsync(categories[index]);
-            System.Threading.Thread.Sleep(1500);
+            if (categoryToDelete == null)
+            {
+                TempData["details"] = "Failed to delete.";
+                TempData["confirmation"] = "fail";
+            }
+            else
+            {
+                try
+                {
+                    var categoriesGroup = await _categoryServiceClient.ListCategoriesAsync(new());
+                    categories = categoriesGroup.SingleCategory.ToList();
+                    var index = categories.FindIndex(c => c.Name == categoryToDelete);
+                    var c = await _categoryServiceClient.RemoveCategoriesAsync(categories[index]);
+                    TempData["details"] = categoryToDelete+" is deleted!";
+                    TempData["confirmation"] = "success";
+
+                }
+                catch
+                {
+                    TempData["details"] = "Failed to delete.";
+                    TempData["confirmation"] = "fail";
+                }
+            }
             return RedirectToPage("./CategoriesPage");
         }
 
+        // Renaming a category.
         public async Task<IActionResult> OnPostUpdate(string newCategory, string oldValue)
         {
-            var categoriesGroup = await _categoryServiceClient.ListCategoriesAsync(new());
-            categories = categoriesGroup.SingleCategory.ToList();
-            EditedCategory editedCategory = new();
-            editedCategory.OldName = categories[Int32.Parse(oldValue)].Name;
-            Console.WriteLine(oldValue);
-            editedCategory.NewName = newCategory;
-            try
+            if (newCategory == null || oldValue == null)
             {
-                var r = await _categoryServiceClient.EditCategoriesAsync(editedCategory);
-                msg.Add("no error1");
+                TempData["details"] = "Empty parameters were given.";
+                TempData["confirmation"] = "fail";
             }
-            catch
+            else
             {
-                msg.Add("error1");
+                var categoriesGroup = await _categoryServiceClient.ListCategoriesAsync(new());
+                categories = categoriesGroup.SingleCategory.ToList();
+                EditedCategory editedCategory = new();
+                editedCategory.OldName = categories[Int32.Parse(oldValue)].Name;
+                editedCategory.NewName = newCategory;
+                try
+                {
+                    var r = await _categoryServiceClient.EditCategoriesAsync(editedCategory);
+                    TempData["confirmation"] = "success";
+                    TempData["details"] = editedCategory.OldName + " is renamed to " + newCategory + "!";
+                }
+                catch (Exception ex)
+                {
+                    TempData["confirmation"] = "fail";
+                    if (ex.Message.Contains("The category name entered already exists."))
+                    {
+                        TempData["details"] = "The category name entered already exists";
+
+                    }
+                    else if (ex.Message.Contains("There is no change in the category value."))
+                    {
+                        TempData["details"] = "There is no change in the category value.";
+                    }
+                    else if (ex.Message.Contains("Empty parameters were given."))
+                    {
+                        TempData["details"] = "Empty parameters were given.";
+                    }
+                    else
+                    {
+                        TempData["details"] = "Failed to edit.";
+                    }
+                }
             }
-            return RedirectToPage("./CategoriesPage", new { messages = msg });
+            return RedirectToPage("./CategoriesPage");
         }
 
+        // Adding a category.
         public async Task<IActionResult> OnPostAdd(string addCategoryName)
         {
-            CategoryItem categoryItem = new();
-            categoryItem.Name = addCategoryName;
-            categoryItem.Id = System.Guid.NewGuid().ToString();
-            try
+            if (addCategoryName == null)
             {
-                var r = await _categoryServiceClient.AddCategoriesAsync(categoryItem);
-                msg.Add("no error2");
+                TempData["details"] = "Empty parameters were given.";
+                TempData["confirmation"] = "fail";
             }
-            catch
+            else
             {
-                msg.Add("error2");
+                CategoryItem categoryItem = new();
+                categoryItem.Name = addCategoryName;
+                categoryItem.Id = System.Guid.NewGuid().ToString();
+                try
+                {
+                    var r = await _categoryServiceClient.AddCategoriesAsync(categoryItem);
+                    TempData["confirmation"] = "success";
+                    TempData["details"] = addCategoryName + " is added!";
+                }
+                catch (Exception ex)
+                {
+                    TempData["confirmation"] = "fail";
+                    if (ex.Message.Contains("This category already exists."))
+                    {
+                        TempData["details"] = "The category name entered already exists";
+                    }
+                    else
+                    {
+                        TempData["details"] = "Failed to add.";
+                    }
+                }
             }
-            return RedirectToPage("./CategoriesPage", new { messages = msg });
+            return RedirectToPage("./CategoriesPage");
         }
     }
 }
--- RazorThree/Pages/ListRecipes.cshtml.cs
+++ RazorThree/Pages/ListRecipes.cshtml.cs
@@ -6,15 +6,18 @@ using Google.Protobuf.WellKnownTypes;
 using System.Net.Http;
 using System.Threading.Tasks.Dataflow;
 using Microsoft.AspNetCore.Hosting.Server;
+using System.Numerics;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorThree.Pages
 {
     public class ListRecipesModel : PageModel
     {
         [BindProperty]
+        public RecipeItem newRecipe { get; set; } = new RecipeItem();
         public List<RecipeItem> recipesList { get; set; } = new List<RecipeItem>();
-        public RecipeItem singleRecipe { get; set; } = new();
-        public RecipeItem newRecipe { get; set; } 
+        public RecipeItem singleRecipe { get; set; } = new RecipeItem();
+
         public List<CategoryItem> categ { get; set; }
         private readonly Recipe.RecipeClient _recipeServiceClient;
         private readonly Category.CategoryClient _categoryServiceClient;
@@ -25,14 +28,114 @@ namespace RazorThree.Pages
             _categoryServiceClient = categoryServiceClient;
         }
 
+        // Listing recipes.
         public async Task OnGet()
         {
             var recipesGroup = await _recipeServiceClient.ListRecipesAsync(new());
             recipesList = recipesGroup.SingleRecipe.ToList();
-            singleRecipe = recipesList[0];
-            recipesList.RemoveAt(0);
+            if (recipesList.Count != 0)
+            {
+                singleRecipe = recipesList[0];
+                recipesList.RemoveAt(0);
+            }
             var categoriesGroup = await _categoryServiceClient.ListCategoriesAsync(new());
             categ = categoriesGroup.SingleCategory.ToList();
+        }
+
+        // Adding a recipe.
+        public async Task<IActionResult> OnPostAdd(List<string> data, string recipeIngredients, string recipeInstructions)
+        {
+
+            if (newRecipe.Title.Trim().Length == 0 || recipeInstructions == null || recipeIngredients == null || data.Count == 0)
+            {
+                TempData["confirmation"] = "fail";
+                TempData["details"] = "Empty parameters.";
+            }
+            else
+            {
+                var ingredientesList = recipeIngredients.Replace("\n", "").Replace("\r", "").Split("• ").ToList();
+                var instructionsList = recipeInstructions.Replace("\n", "").Replace("\r", "").Split("• ").ToList();
+                ingredientesList.RemoveAt(0);
+                instructionsList.RemoveAt(0);
+                newRecipe.Ingredients.AddRange(ingredientesList);
+                newRecipe.Instructions.AddRange(instructionsList);
+                newRecipe.Categories.AddRange(data);
+                newRecipe.Id = System.Guid.NewGuid().ToString();
+                try
+                {
+                    var r = await _recipeServiceClient.AddRecipeAsync(newRecipe);
+                    TempData["confirmation"] = "success";
+                    TempData["details"] = newRecipe.Title + " is added!";
+                }
+                catch (Exception ex)
+                {
+                    TempData["confirmation"] = "fail";
+                    if (ex.Message.Contains("Empty parameters."))
+                    {
+                        TempData["details"] = "Empty parameters.";
+                    }
+                    else
+                    {
+                        TempData["details"] = "Failed to add recipe.";
+                    }
+                }
+            }
+            return RedirectToPage("./ListRecipes");
+        }
+
+        // Deleting a recipe.
+        public async Task<IActionResult> OnPostDelete(string recipeToDelete)
+        {
+            if (recipeToDelete == null)
+            {
+                TempData["details"] = "Failed to delete.";
+                TempData["confirmation"] = "fail";
+            }
+            else
+            {
+                try
+                {
+                    var recipesGroup = await _recipeServiceClient.ListRecipesAsync(new());
+                    recipesList = recipesGroup.SingleRecipe.ToList();
+                    var recipeToRemove = recipesList.Find(r => r.Id == recipeToDelete);
+                    var r = await _recipeServiceClient.RemoveRecipeAsync(recipeToRemove);
+                    TempData["details"] = recipeToRemove.Title + " is deleted!";
+                    TempData["confirmation"] = "success";
+                }
+                catch
+                {
+                    TempData["details"] = "Failed to delete.";
+                    TempData["confirmation"] = "fail";
+                }
+            }
+            return RedirectToPage("./ListRecipes");
+        }
+
+        // Editing a recipe.
+        public async Task<IActionResult> OnPostUpdate(string id, string instructions, List<string> categories, string ingredients, string newTitle)
+        {
+            var ingredientesList = ingredients.Replace("\n", "").Replace("\r", "").Split("• ").ToList();
+            ingredientesList.RemoveAt(0);
+            var instructionsList = instructions.Replace("\n", "").Replace("\r", "").Split("• ").ToList();
+            instructionsList.RemoveAt(0);
+            RecipeItem recipeToEdit = new();
+            recipeToEdit.Ingredients.AddRange(ingredientesList);
+            recipeToEdit.Instructions.AddRange(instructionsList);
+            recipeToEdit.Id = id;
+            recipeToEdit.Title = newTitle;
+            recipeToEdit.Categories.AddRange(categories);
+            try
+            {
+                var r = await _recipeServiceClient.EditRecipeAsync(recipeToEdit);
+                TempData["confirmation"] = "success";
+                TempData["details"] = "recipe is edited!";
+            }
+            catch
+            {
+                TempData["confirmation"] = "fail";
+                TempData["details"] = "Failed to edit recipe";
+            }
+            return RedirectToPage("./ListRecipes");
         }
     }
 }
